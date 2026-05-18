7e53e9bf39d085f06b53010d2f56a16dba1d27cc
Nguyen Quang Minh (NQM) <nguyenquangminh391@gmail.com>
2026-05-18 10:35:15 +0700

    chore: bump version to 1.7.2 (13)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "app.pwhs.universalinstaller"
         minSdk = 24
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.7.1"
+        versionCode = 13
+        versionName = "1.7.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
