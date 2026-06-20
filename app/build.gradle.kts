1a853e1884aa2d843ecf9dc475f9e1ee63c7b5ea
Nguyen Quang Minh (NQM) <nguyenquangminh391@gmail.com>
2026-06-20 21:11:20 +0700

    chore: bump version to 1.9.4 (24)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "app.pwhs.universalinstaller"
         minSdk = 24
         targetSdk = 36
-        versionCode = 23
-        versionName = "1.9.3"
+        versionCode = 24
+        versionName = "1.9.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
